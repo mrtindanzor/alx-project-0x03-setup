cd39573c243df57472f71b253e05a9b7bdaaf87c
Mr. Tindanzor <ktindanzor@gmail.com>
2025-11-20 18:26:59 +0000

    added base files, copy and paste 😂😂

---
+++ alx-project-0x03/components/layouts/Layout.tsx
@@ -0,0 +1,15 @@
+import { type LayoutProps } from "@/interfaces/index";
+import Footer from "./Footer";
+import Header from "./Header";
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
